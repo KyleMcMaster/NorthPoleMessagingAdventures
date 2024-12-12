305736c945dab5fca38ddda1ade412e7b8d8dc39
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2024-12-11 20:15:42 -0500

    Add services to aspire host

--- src/NorthPole.AspireHost/Program.cs
+++ src/NorthPole.AspireHost/Program.cs
@@ -1,5 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.NorthPole_Web>("web");
+builder.AddProject<Projects.NorthPole_Web>("Web");
+
+builder.AddProject<Projects.NorthPole_SantasInbox>("Santas-Inbox");
+
+builder.AddProject<Projects.NorthPole_SantasWorkshop>("Santas-Workshop");
+
+builder.AddProject<Projects.NorthPole_SantasSleigh>("Santas-Sleigh");
+
+builder.AddProject<Projects.WorldWide_Homes>("Chimneys-Worldwide");
 
 builder.Build().Run();
