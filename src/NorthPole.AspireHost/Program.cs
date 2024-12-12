cd4844dbc96b2631515f4455e9de09b73fe91068
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2024-12-11 21:13:21 -0500

    update web aspire resource name

--- src/NorthPole.AspireHost/Program.cs
+++ src/NorthPole.AspireHost/Program.cs
@@ -1,6 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.NorthPole_Web>("Web");
+builder.AddProject<Projects.NorthPole_Web>("North-Pole-Api");
 
 builder.AddProject<Projects.NorthPole_SantasInbox>("Santas-Inbox");
 
