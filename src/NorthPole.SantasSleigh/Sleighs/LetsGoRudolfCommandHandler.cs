b74a249a2ce43c5fd54ff6a351926ff9ab34c410
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2024-12-17 15:06:14 -0500

    Implement LetsGoRudolfCommandHandler

--- src/NorthPole.SantasSleigh/Sleighs/LetsGoRudolfCommandHandler.cs
+++ src/NorthPole.SantasSleigh/Sleighs/LetsGoRudolfCommandHandler.cs
@@ -2,24 +2,17 @@ using Ardalis.SharedKernel;
 using NorthPole.Core.SantasSleigh;
 
 namespace NorthPole.SantasSleigh.SantasSleigh;
-public class LetsGoRudolfCommandHandler : IHandleMessages<LetsGoRudolfCommand>
+
+public class LetsGoRudolfCommandHandler(ILogger<LetsGoRudolfCommandHandler> logger, IRepository<Sleigh> repository) : IHandleMessages<LetsGoRudolfCommand>
 {
-  private readonly ILogger<LetsGoRudolfCommandHandler> _logger;
-  private readonly IRepository<Sleigh> _repository;
+  private readonly ILogger<LetsGoRudolfCommandHandler> _logger = logger;
+  private readonly IRepository<Sleigh> _repository = repository;
 
-  public LetsGoRudolfCommandHandler(ILogger<LetsGoRudolfCommandHandler> logger, IRepository<Sleigh> repository)
-  {
-    _logger = logger;
-    _repository = repository;
-  }
-
-  public Task Handle(LetsGoRudolfCommand message, IMessageHandlerContext context)
+  public async Task Handle(LetsGoRudolfCommand message, IMessageHandlerContext context)
   {
     _logger.LogInformation("Received command to unleash Rudolf's true potential!");
-    // TODO: var sleigh = await _repository.GetByIdAsync(message.SleighId);
-    Sleigh _sleigh = new([]);
-    _sleigh.Takeoff();
+    var sleigh = await _repository.SingleOrDefaultAsync(new SantasSleighSpecification(), context.CancellationToken);
+    sleigh!.Takeoff();
     _logger.LogInformation("Rudolf has saved the day!");
-    return Task.CompletedTask;
   }
 }
